d0451b5b813d10268a3f24e7239f96c6fd32460f
subclinical <kaiwen006@gmail.com>
2020-01-12 13:25:13 -0500

    explicit pg config

--- index.js
+++ index.js
@@ -5,7 +5,13 @@ const rateLimiter = require('./rateLimiter')
 const app = express()
 // configs come from standard PostgreSQL env vars
 // https://www.postgresql.org/docs/9.6/static/libpq-envars.html
-const pool = new pg.Pool()
+const pool = new pg.Pool({
+  user: process.env.PGUSER,
+  host: process.env.PGHOST,
+  database: process.env.PGDATABASE,
+  password: process.env.PGPASSWORD,
+  port: process.env.PGPORT,
+})
 
 const queryHandler = (req, res, next) => {
   pool.query(req.sqlQuery).then((r) => {
